95523ea59afb9de5e2f5e33fa9369454611ba1c2
AungPhoneKhant <akha0030@student.monash.edu>
2020-08-03 16:37:43 +1000

    Moved pages to separate file.

---
+++ src/components/pages.js
@@ -0,0 +1,22 @@
+const pages = [
+    {
+        key: "header_use_state",
+        to: "./use-state",
+        label: "Direct State Mutation",
+        completed: true,
+    },
+    {
+        key: "header_react_keys",
+        to: "./react-keys",
+        label: "React Keys",
+        completed: true,
+    },
+    {
+        key: "header_use_memo",
+        to: "./use-memo",
+        label: "Use Memo Hook",
+        completed: false,
+    },
+];
+
+export default pages;
